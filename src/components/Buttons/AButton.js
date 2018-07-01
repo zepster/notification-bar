fb5cdf959011d487d499c7452ed9aab55077749e
Aleksey Smirnov <aleksey.exe@gmail.com>
2018-07-01 22:40:17 +0400

    submit btn props

--- src/components/Buttons/AButton.js
+++ src/components/Buttons/AButton.js
@@ -4,6 +4,8 @@ import './a-button.css';
 const PLACEHOLDERS = {
   Text: '{%TEXT%}',
   buttonBackgroundColor: '{%BG_COLOR%}',
+  buttonUrl: '{%BUTTON_URL%}',
+  openNewTab: '{%OPEN_NEW_TAB%}',
 };
 
 /**
@@ -17,7 +19,7 @@ export default class AButton {
    * @param {object} options.Text - Text component
    * @param {object} options.config - button config
    * @param {string} options.config.buttonUrl - button Url
-   * @param {string} options.config.openNewTab - Clicking button will open in new tab
+   * @param {boolean} options.config.openNewTab - Clicking button will open in new tab
    * @param {boolean} options.config.wiggleButton - true - “Wiggle” button animation enabled (​https://daneden.github.io/animate.css/)​
    * @param {boolean} options.config.buttonBackgroundColor - true - “Wiggle” button animation enabled (​https://daneden.github.io/animate.css/)​
    */
@@ -25,6 +27,8 @@ export default class AButton {
     this.placeholders = {
       [ PLACEHOLDERS.Text ] : options.Text,
       [ PLACEHOLDERS.buttonBackgroundColor ] : options.config.buttonBackgroundColor,
+      [ PLACEHOLDERS.buttonUrl ] : options.config.buttonUrl,
+      [ PLACEHOLDERS.openNewTab ] : options.config.openNewTab ? '_blank' : '',
     };
   }
 
